97f7e39ca6d53733313dda24c8c78b027844f6eb
fatcatdog <duchenjacob@gmail.com>
2018-03-28 15:11:07 -0400

    freindly chat with firebase auth and storage set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';                        //new
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 final ThemeData kIOSTheme = new ThemeData(
   primarySwatch: Colors.orange,
@@ -13,8 +24,31 @@ final ThemeData kDefaultTheme = new ThemeData(
   accentColor: Colors.orangeAccent[400],
 );
 
+final googleSignIn = new GoogleSignIn();
+final analytics = new FirebaseAnalytics();
+final auth = FirebaseAuth.instance;
+final reference = FirebaseDatabase.instance.reference().child('messages');
+
 void main() {
   runApp(new FriendlychatApp());
+}
+
+Future<Null> _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null)
+    user = await googleSignIn.signInSilently();
+  if (user == null) {
+    user = await googleSignIn.signIn();
+    analytics.logLogin();
+  }
+  if (await auth.currentUser() == null) {
+    GoogleSignInAuthentication credentials =
+    await googleSignIn.currentUser.authentication;
+    await auth.signInWithGoogle(
+      idToken: credentials.idToken,
+      accessToken: credentials.accessToken,
+    );
+  }
 }
 
 class FriendlychatApp extends StatelessWidget {
@@ -22,167 +56,176 @@ class FriendlychatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Friendlychat",
-      theme: defaultTargetPlatform == TargetPlatform.iOS         //new
-          ? kIOSTheme                                              //new
-          : kDefaultTheme,                                         //new
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
       home: new ChatScreen(),
     );
   }
 }
 
-class ChatScreen extends StatefulWidget {//modified
-  @override                                                        //new
-  State createState() => new ChatScreenState();                    //new
+@override
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.snapshot, this.animation});
+  final DataSnapshot snapshot;
+  final Animation animation;
+
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+          parent: animation, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(backgroundImage: new NetworkImage(snapshot.value['senderPhotoUrl'])),
+            ),
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(
+                      snapshot.value['senderName'],
+                      style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: snapshot.value['imageUrl'] != null ?
+                    new Image.network(
+                      snapshot.value['imageUrl'],
+                      width: 250.0,
+                    ) :
+                    new Text(snapshot.value['text']),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
-  //new
-  final List<ChatMessage> _messages = <ChatMessage>[];             // new
-  final TextEditingController _textController = new TextEditingController();
-  bool _isComposing = false;                                      //new
-
+class ChatScreen extends StatefulWidget {
   @override
-  void dispose() {                                                   //new
-    for (ChatMessage message in _messages)                           //new
-      message.animationController.dispose();                         //new
-    super.dispose();                                                 //new
-  }
-  //new
-  void _handleSubmitted(String text) {
-    _textController.clear();
+  State createState() => new ChatScreenState();
+}
 
-    setState(() {                                                    //new
-      _isComposing = false;                                          //new
-    });
-
-    ChatMessage message = new ChatMessage(
-      text: text,
-      animationController: new AnimationController(                  //new
-        duration: new Duration(milliseconds: 700),                   //new
-        vsync: this,                                                 //new
-      ),                                                             //new
-    );                                                               //new
-    setState(() {
-      _messages.insert(0, message);
-    });
-    message.animationController.forward();                           //new
-  }
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Friendlychat"),                                 //modified
-        elevation:
-        Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0, //new
-      ),
-      body: new Container(                                             //modified
-        child: new Column(
-        children: <Widget>[                                         //new
-          new Flexible(                                             //new
-            child: new ListView.builder(                            //new
-              padding: new EdgeInsets.all(8.0),                     //new
-              reverse: true,                                        //new
-              itemBuilder: (_, int index) => _messages[index],      //new
-              itemCount: _messages.length,                          //new
-            ),                                                      //new
-          ),                                                        //new
-          new Divider(height: 1.0),                                 //new
-          new Container(                                            //new
-            decoration: new BoxDecoration(
-                color: Theme.of(context).cardColor),                  //new
-            child: _buildTextComposer(),                       //modified
-          ),                                                        //new
-        ],                                                          //new
-      ),
-        decoration: Theme.of(context).platform == TargetPlatform.iOS //new
-            ? new BoxDecoration(                                     //new
-          border: new Border(                                  //new
-            top: new BorderSide(color: Colors.grey[200]),      //new
-          ),                                                   //new
-        )                                                      //new
-            : null),             //new
-    );
+        appBar: new AppBar(
+          title: new Text("Friendlychat"),
+          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: new Column(children: <Widget>[
+          new Flexible(
+            child: new FirebaseAnimatedList(
+              query: reference,
+              sort: (a, b) => b.key.compareTo(a.key),
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, DataSnapshot snapshot, Animation<double> animation) {
+                return new ChatMessage(
+                    snapshot: snapshot,
+                    animation: animation
+                );
+              },
+            ),
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration:
+            new BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ]));
   }
 
   Widget _buildTextComposer() {
-    return new IconTheme(                                            //new
-        data: new IconThemeData(color: Theme.of(context).accentColor), //new
-    child: new Container(
-    margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new Row(                                            //new
-        children: <Widget>[                                      //new
-          new Flexible(                                          //new
-            child: new TextField(
-              controller: _textController,
-              onChanged: (String text) {          //new
-                setState(() {                     //new
-                  _isComposing = text.length > 0; //new
-                });                               //new
-              },
-              onSubmitted: _handleSubmitted,
-              decoration: new InputDecoration.collapsed(
-                  hintText: "Send a message"),
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(children: <Widget>[
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.photo_camera),
+                  onPressed: () async {
+                    await _ensureLoggedIn();
+                    File imageFile = await ImagePicker.pickImage();
+                    int random = new Random().nextInt(100000);
+                    StorageReference ref =
+                    FirebaseStorage.instance.ref().child("image_$random.jpg");
+                    StorageUploadTask uploadTask = ref.put(imageFile);
+                    Uri downloadUrl = (await uploadTask.future).downloadUrl;
+                    _sendMessage(imageUrl: downloadUrl.toString());
+                  }
+              ),
             ),
-          ),
-          new Container(                                                 //new
-            margin: new EdgeInsets.symmetric(horizontal: 4.0),
-            child: Theme.of(context).platform == TargetPlatform.iOS ?  //modified
-            new CupertinoButton(                                       //new
-              child: new Text("Send"),                                 //new
-                onPressed: _isComposing                                  //new
-                    ? () =>  _handleSubmitted(_textController.text)      //new
-                    : null,) :                                         //new
-            new IconButton(                                       //new
-                icon: new Icon(Icons.send),                                //new
-              onPressed: _isComposing
-                  ? () => _handleSubmitted(_textController.text)    //modified
-                  : null,
-            ),//new
-          ),   //new
-        ],                                                        //new
-      ),
-    ),//new
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: _handleSubmitted,
+                decoration:
+                new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? new CupertinoButton(
+                  child: new Text("Send"),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_textController.text)
+                      : null,
+                )
+                    : new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_textController.text)
+                      : null,
+                )),
+          ]),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+              border:
+              new Border(top: new BorderSide(color: Colors.grey[200])))
+              : null),
     );
   }
-}
 
-const String _name = "Your Name";
-
-class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text, this.animationController});         //modified
-  final String text;
-  final AnimationController animationController;                   //new
-  @override
-  Widget build(BuildContext context) {
-    return new SizeTransition(                                    //new
-        sizeFactor: new CurvedAnimation(                              //new
-        parent: animationController, curve: Curves.easeOut),      //new
-    axisAlignment: 0.0,                                           //new
-    child: new Container(                                    //modified
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])),
-          ),
-          new Expanded(
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(_name, style: Theme.of(context).textTheme.subhead),
-                new Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: new Text(text),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    )
-    );
+  Future<Null> _handleSubmitted(String text) async {
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    await _ensureLoggedIn();
+    _sendMessage(text: text);
   }
+
+  void _sendMessage({ String text, String imageUrl }) {
+    reference.push().set({
+      'text': text,
+      'imageUrl': imageUrl,
+      'senderName': googleSignIn.currentUser.displayName,
+      'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
+    });
+    analytics.logEvent(name: 'send_message');
+  }
+
 }
